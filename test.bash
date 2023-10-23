347941d3ce33a7f32355f0201f887e76ae32111e
kouta furukawa <furuko1030@gmail.jp>
2023-10-23 13:31:02 +0900

    update

--- test.bash
+++ test.bash
@@ -9,20 +9,21 @@ ng () {
 
 res=0
 
-### I/O TEST ###
-out=$(seq 5 | ./plus)
-++ seq 5
-++ ./plus
-+ out=15
-[ "${out}" = 15 ] || ng ${LINENO}
-+'[' 15 = 15 ']'
+### I/O  ###
+ out=$(seq 5 | ./plus)
+  [ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+ out=$(echo あ | ./plus)
+  [ "$" = 1 ]       || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+
+ out=$(echo | ./plus) #空文字
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
-+'[' 0 = 0 ']'
-+ echo OK
-OK
-
 exit $res
-+exit 0
+
 
 
