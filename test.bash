b078e71252c40f99677ed8a382ced55e54e0bd1a
kouta furukawa <furuko1030@gmail.jp>
2023-11-24 08:15:23 +0900

    H

---
+++ test.bash
@@ -0,0 +1,27 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+# SPDX-License-Identifier: BSD-3-Clause
+　
+ng () {
+      echo NG at Line $1
+      res=1
+}
+　
+res=0
++ res=0
+
+  ### I/O ###
+  out=$(seq 5 | ./plus_stdin)
+  [ "${out}" = 15 ] || ng ${LINENO}
+    　 
+  ### STRANGE INPUT ###
+  out=$(echo あ | ./plus_stdin)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+    　 
+  out=$(echo | ./plus_stdin) #空文字
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+    　 
+[ "$res" = 0 ] && echo OK
+  exit $res
